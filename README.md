40599026ce5d6bd3aa284f2e7582e8cfa29c0409
Bence Utasi <butasi@diligent.com>
2022-10-21 11:22:20 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# fastify-poc
+
+Usage:
+Install packages `npm install`
+Run app `npm start`
---
+++ server.js
@@ -0,0 +1,14 @@
+const fastify = require('fastify')({ logger: true });
+
+const PORT = 3000;
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
